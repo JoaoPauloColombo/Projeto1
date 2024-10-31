e5838842a53324a78e3834afa6ffb9bb089e8b48
Ettore <joao.colombo@edu.fecap.br>
2024-10-31 00:07:16 -0300

    Backend Antigo

--- src/Backend/src/middlewares/ValidateUser.js
+++ src/Backend/src/middlewares/ValidateUser.js
@@ -1,40 +1,40 @@
-// Nome, email e senha
-const validateUser  = (req, res, next) => {
-  console.log('Corpo da requisição no middleware:', req.body); // Log para depuração
+//Nome , email e senha
 
-  const { nome, email, senha } = req.body;
+const validateUser = (req, res, next) => {
+    const { nome, email, senha } = req.body;
+    console.log('Valor do campo "nome":', nome);
 
-  if (!nome) {
-    return res.status(400).json({
-      msg: "O campo 'nome' é obrigatório",
-    });
-  }
+    if (!nome) {
+      return res.status(400).json({
+        msg: "O campo 'nome' é obrigatório",
+      });
+    }
+  
+    if (!email) {
+      return res.status(400).json({
+        msg: "O campo 'email' é obrigatório",
+      });
+    }
+  
+    if (!senha) {
+      return res.status(400).json({
+        msg: "O campo 'senha' é obrigatório",
+      });
+    }
+  
+    return next();
+  };
 
-  if (!email) {
-    return res.status(400).json({
-      msg: "O campo 'email' é obrigatório",
-    });
-  }
+const validateUserId = (req,res,next) => {
+    const {id} = req.params;
 
-  if (!senha) {
-    return res.status(400).json({
-      msg: "O campo 'senha' é obrigatório",
-    });
-  }
+    if(!id) {
+        return res.status(400).json({
+            msg:"Parametro faltando",
+        });
+    }
 
-  return next();
+    return next();
 };
 
-const validateUserId = (req, res, next) => {
-  const { id } = req.params;
-
-  if (!id) {
-    return res.status(400).json({
-      msg: "Parâmetro faltando",
-    });
-  }
-
-  return next();
-};
-
-module.exports = { validateUser , validateUserId };
+module.exports = {validateUser,validateUserId};
