0a4af37d83e67fa2a9ea7bc4a97dc14ee712e66c
Ettore <joao.colombo@edu.fecap.br>
2024-11-16 14:46:05 -0300

    Arrumando o authenticate

--- src/Backend/src/router/routerComentario.js
+++ src/Backend/src/router/routerComentario.js
@@ -1,16 +1,16 @@
 const { Router } = require("express");
 const ComentarioController = require("../controller/ComentarioController");
 const { validateComentario, validateComentarioId } = require("../middlewares/ValidateComentario");
-const { authenticate } = require("../middlewares/authenticateToken");
+const authenticateToken = require("../middlewares/authenticateToken");
 
 const router = Router();
 
 // Rotas de Comentários
-router.post('/', authenticate, validateComentario, (req, res) => {
+router.post('/', authenticateToken, validateComentario, (req, res) => {
     ComentarioController.create(req, res);
 });
 
-router.put('/:id', authenticate, validateComentario, validateComentarioId, (req, res) => {
+router.put('/:id', authenticateToken, validateComentario, validateComentarioId, (req, res) => {
     ComentarioController.update(req, res);
 });
 
@@ -22,7 +22,7 @@ router.get('/:id', validateComentarioId, (req, res) => {
     ComentarioController.getOne(req, res);
 });
 
-router.delete('/:id', authenticate, validateComentarioId, (req, res) => {
+router.delete('/:id', authenticateToken, validateComentarioId, (req, res) => {
     ComentarioController.delete(req, res);
 });
 
--- src/Backend/src/router/routerCoordenadas.js
+++ src/Backend/src/router/routerCoordenadas.js
@@ -1,16 +1,16 @@
 const { Router } = require("express");
 const CoordenadasController = require("../controller/CoordenadasController");
 const { validateCoordenadas, validateCoordenadasId } = require("../middlewares/ValidateCoordenadas");
-const { authenticate } = require("../middlewares/authenticateToken");
+const authenticateToken = require("../middlewares/authenticateToken");
 
 const router = Router();
 
 // Rotas de Coordenadas
-router.post('/', authenticate, validateCoordenadas, (req, res) => {
+router.post('/', authenticateToken, validateCoordenadas, (req, res) => {
     CoordenadasController.create(req, res);
 });
 
-router.put('/:id', authenticate, validateCoordenadasId, validateCoordenadas, (req, res) => {
+router.put('/:id', authenticateToken, validateCoordenadasId, validateCoordenadas, (req, res) => {
     CoordenadasController.update(req, res);
 });
 
@@ -22,7 +22,7 @@ router.get('/:id', validateCoordenadasId, (req, res) => {
     CoordenadasController.getOne(req, res);
 });
 
-router.delete('/:id', authenticate, validateCoordenadasId, (req, res) => {
+router.delete('/:id', authenticateToken, validateCoordenadasId, (req, res) => {
     CoordenadasController.delete(req, res);
 });
 
