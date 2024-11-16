937023f91c16de3537befdf0dd6c8bc602da791c
Ettore <joao.colombo@edu.fecap.br>
2024-11-16 15:54:45 -0300

    Teste

--- src/Backend/src/router/routerCoordenadas.js
+++ src/Backend/src/router/routerCoordenadas.js
@@ -1,16 +1,15 @@
 const { Router } = require("express");
 const CoordenadasController = require("../controller/CoordenadasController");
 const { validateCoordenadas, validateCoordenadasId } = require("../middlewares/ValidateCoordenadas");
-const authenticateToken = require("../middlewares/authenticateToken");
 
 const router = Router();
 
 // Rotas de Coordenadas
-router.post('/', authenticateToken, validateCoordenadas, (req, res) => {
+router.post('/', validateCoordenadas, (req, res) => {
     CoordenadasController.create(req, res);
 });
 
-router.put('/:id', authenticateToken, validateCoordenadasId, validateCoordenadas, (req, res) => {
+router.put('/:id', validateCoordenadasId, validateCoordenadas, (req, res) => {
     CoordenadasController.update(req, res);
 });
 
@@ -22,7 +21,7 @@ router.get('/:id', validateCoordenadasId, (req, res) => {
     CoordenadasController.getOne(req, res);
 });
 
-router.delete('/:id', authenticateToken, validateCoordenadasId, (req, res) => {
+router.delete('/:id', validateCoordenadasId, (req, res) => {
     CoordenadasController.delete(req, res);
 });
 
